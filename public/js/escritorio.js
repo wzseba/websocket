4dd8aee8e52445fb58f5e211072e586f743d3971
wzseba <wzseba@gmail.com>
2024-04-07 11:15:20 -0300

    add: en desarrollo manejo del dom y comunicacion con sockets

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,41 @@
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+const atendiendo = document.querySelector('small');
+const lblPendientes = document.querySelector('#lblPendientes');
 
+// Tomo parametro de la url actual
+const searchParams = new URLSearchParams(window.location.search);
 
-console.log('Escritorio HTML');
+// Verifico que en la url venga el parametro escritorio
+if (!searchParams.has('escritorio')) {
+  window.location = 'index.html';
+  throw new Error('El escritorio es obligatorio');
+}
+
+// Obtengo informacion del escritorio
+const escritorio = searchParams.get('escritorio');
+
+// Asignamos el escritorio al DOM
+lblEscritorio.innerText = escritorio.replace('e', 'E');
+
+const socket = io();
+
+socket.on('connect', () => {
+  btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+  btnAtender.disabled = true;
+});
+
+// Recibo desde el server el ultimo ticket
+socket.on('escritorio-actual', ultimo => {
+  atendiendo.innerText = 'Ticket ' + ultimo;
+});
+
+btnAtender.addEventListener('click', () => {
+  //   socket.emit('siguente-ticket', null, ticket => {
+  //     lblNuevoTicket.innerText = ticket;
+  //   });
+});
--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,24 @@
+// Referencias HTML
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const btnCrear = document.querySelector('button');
 
+const socket = io();
 
-console.log('Nuevo Ticket HTML');
+socket.on('connect', () => {
+  btnCrear.disabled = false;
+});
+
+socket.on('disconnect', () => {
+  btnCrear.disabled = true;
+});
+
+// Recibo desde el server el ultimo ticket
+socket.on('ultimo-ticket', ultimo => {
+  lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+});
+
+btnCrear.addEventListener('click', () => {
+  socket.emit('siguente-ticket', null, ticket => {
+    lblNuevoTicket.innerText = ticket;
+  });
+});
