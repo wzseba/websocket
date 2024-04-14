c168a89000b22e98ed75d5c6e142e5728d06c7b5
wzseba <wzseba@gmail.com>
2024-04-13 23:09:57 -0300

    add: desarrollo de logica atender-ticket

--- sockets/controller.js
+++ sockets/controller.js
@@ -6,11 +6,40 @@ const socketController = socket => {
   // Emito el ultimo ticket hacia el front
   socket.emit('ultimo-ticket', ticketControl.ultimo);
 
+  // Emito los 4 ultimos tickets
+  socket.emit('estado-actual', ticketControl.ultimos4);
+
   socket.on('siguente-ticket', (payload, callback) => {
     const siguente = ticketControl.siguiente();
     callback(siguente);
 
     // Notificar que hay ticket pendiente
+  });
+
+  socket.on('atender-ticket', ({ escritorio }, callback) => {
+    // Validar que haya escritorio
+    if (!escritorio) {
+      callback({
+        ok: false,
+        msg: 'Escritorio obligatorio',
+      });
+    }
+
+    // Ticket que tengo que atender
+    const ticket = ticketControl.atenderTicket(escritorio);
+
+    // Validar que haya ticket
+    if (!ticket) {
+      callback({
+        ok: false,
+        msg: 'No hay ticket',
+      });
+    } else {
+      callback({
+        ok: true,
+        ticket,
+      });
+    }
   });
 };
 
